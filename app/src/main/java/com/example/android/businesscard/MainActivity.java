08d2f7db01e84d40910634b4b8fa7ed1c08e9423
EdmondN <33865566+EdmondN@users.noreply.github.com>
2018-02-21 10:48:59 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/android/businesscard/MainActivity.java
+++ app/src/main/java/com/example/android/businesscard/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.android.businesscard;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    TextView HyperLink;
+    Spanned Text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HyperLink = (TextView)findViewById(R.id.textView1);
+
+        Text = Html.fromHtml ("<a href='https://www.udacity.com//'>Udacity</a>");
+
+        HyperLink.setMovementMethod(LinkMovementMethod.getInstance());
+        HyperLink.setText(Text);
+
+        findViewById(R.id.textView4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialContactPhone("6505555555");
+            }
+        });
+
+
+    }
+
+
+    private void dialContactPhone(final String phoneNumber) {
+        startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
     }
 }
